c9f36a5f4138df276a27b6e2bd1236894511a5a2
Aleksei Serdiuk <aleksey.serduyk@gmail.com>
2023-03-20 14:28:07 +0600

    innitial commit

---
+++ WheatherForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WheatherForecast
+//
+//  Created by Алексей Сердюк on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
