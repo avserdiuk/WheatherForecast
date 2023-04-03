b1d81ceb7a1b8dbe978f164ff1ff4a79cedc42ba
Aleksei Serdiuk <aleksey.serduyk@gmail.com>
2023-04-03 18:32:38 +0600

    added location request after first open

--- WheatherForecast/Models/LocationManager.swift
+++ WheatherForecast/Models/LocationManager.swift
@@ -8,46 +8,22 @@
 import Foundation
 import CoreLocation
 
-class LocationManager : NSObject, CLLocationManagerDelegate{
+class LocationManager {
 
     private lazy var locationManager : CLLocationManager = {
         let manager = CLLocationManager()
-        manager.delegate = self
         return manager
     }()
 
-        func locationManagerDidChangeAuthorization(
-            _ manager: CLLocationManager
-        ) {
-            switch manager.authorizationStatus {
-            case .authorizedAlways, .authorizedWhenInUse:
-                manager.requestLocation()
-            case .denied, .restricted:
-                print("Определение локации невозможно")
-            case .notDetermined:
-                print("Определение локации не запрошено")
-            @unknown default:
-                fatalError()
-            }
-        }
-
-        func locationManager(
-            _ manager: CLLocationManager,
-            didUpdateLocations locations: [CLLocation]
-        ) {
-            if let location = locations.first {
-//                let controller = PageViewController()
-//                controller.currentCoords = location.coordinate
-//                navigationController?.pushViewController(controller, animated: true)
-            } else {
-                print("Не удалось получить координаты")
-            }
-        }
-
-        func locationManager(
-            _ manager: CLLocationManager,
-            didFailWithError error: Error
-        ) {
-            // Handle failure to get a user’s location
-        }
+    func getRequest(){
+        locationManager.requestAlwaysAuthorization()
     }
+
+    func getStatus(){
+        print(locationManager.authorizationStatus.rawValue)
+    }
+
+    func getPosition(){
+        locationManager.requestLocation()
+    }
+}
